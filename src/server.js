252a8f2eb76087d8bd0170dae08c6657dcf7b96b
kotenkoI <iryna.herashchenko@gmail.com>
2024-08-15 00:59:08 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,26 +1,23 @@
 import express from 'express';
-const express = require('express');
-const cors = require('cors');
-const pino = require('express-pino-logger')();
+import cors from 'cors';
+import { createLogger } from 'pino';
 
-function setupServer() {
+const setupServer = () => {
   const app = express();
 
   app.use(cors());
-  app.use(pino);
+  const logger = createLogger();
 
+  
   app.use((req, res, next) => {
     res.status(404).json({ message: 'Not found' });
   });
 
+  
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    logger.info(`Server is running on port ${PORT}`);
   });
-}
+};
 
-module.exports = { setupServer };
-const contactsRouter = require('./routes/contacts');
-
-app.use('/contacts', contactsRouter);
-
+export { setupServer };
