82be4b75d283aeee7d472e0db786a2c76f25ccce
kotenkoI <iryna.herashchenko@gmail.com>
2024-08-15 01:17:07 +0300

    Update initMongoConnection.js

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -2,11 +2,14 @@ import mongoose from 'mongoose';
 
 export const initMongoConnection = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('Database connected successfully');
+    const user = process.env.MONGODB_USER;
+    const pwd = process.env.MONGODB_PASSWORD;
+    const url = process.env.MONGODB_URL;
+    const db = process.env.MONGODB_DB;
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`,
+    );
+    console.log('Database connection successfully established!');
   } catch (error) {
     console.error('Database connection failed:', error);
     process.exit(1);
