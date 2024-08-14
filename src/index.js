228433792c424dd7b77bfabcaaf1b3d03cd30a4a
kotenkoI <iryna.herashchenko@gmail.com>
2024-08-15 00:50:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 const { setupServer } = require('./server');
-
-setupServer();
-
 const { initMongoConnection } = require('./db/initMongoConnection');
-const { setupServer } = require('./server');
 
 (async () => {
   await initMongoConnection();
